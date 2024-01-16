c59d7e1515fb29f626adf598d1d9bbddb2cc8f29
Verzys <lucas.saliorliange.pro@gmail.com>
2024-01-16 22:05:30 +0100

    Ajout du début de l'html pour chaque site et des routes et du css

--- Api/app/Flask/app.py
+++ Api/app/Flask/app.py
@@ -7,13 +7,43 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def home():
     """
     Permet de définir la page d'accueil
     :return: render_template retourne la page html associée se trouvant dans le
     dossier 'templates'
     """
     return render_template('index.html')
+
+
+@app.route('/search_model')
+def search():
+    """
+    Permet de définir la page de recherche
+    :return: render_template retourne la page html associée se trouvant dans le
+    dossier 'templates'
+    """
+    return render_template('search_model.html')
+
+
+@app.route('/comparaison')
+def comparaison():
+    """
+    Permet de définir la page de comparaison
+    :return: render_template retourne la page html associée se trouvant dans le
+    dossier 'templates'
+    """
+    return render_template('comparaison.html')
+
+
+@app.route('/visualisation')
+def visualisation():
+    """
+    Permet de définir la page de visualisation (graphique)
+    :return: render_template retourne la page html associée se trouvant dans le
+    dossier 'templates'
+    """
+    return render_template('visualisation.html')
 
 
 if __name__ == '__main__':
