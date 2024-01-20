bb1945efdc921693ca9bdb94b1a02e1b87267f22
Verzys <lucas.saliorliange.pro@gmail.com>
2024-01-20 01:27:36 +0100

    Ajout de la fonction de filtrer

--- Api/ElasticSearch/elasticsearch_functions.py
+++ Api/ElasticSearch/elasticsearch_functions.py
@@ -2,11 +2,84 @@ from .elasticsearch_app import es
 from elasticsearch.helpers import bulk
 
 
+def generate_data(documents):
+    """
+    Permet de générer les données pour l'indexation
+    :param documents: issus du mongodb
+    """
+    for document in documents:
+        yield {
+            # Définit le nom de l'index
+            "_index": "porsches",
+            # Définit le type de document
+            "_types": "porsche",
+            # Définit le document avec les données qui sera indexé
+            "_source": {k: v if v else None for k, v in document.items()},
+        }
+
+
+def clear_es_client(es_client=es, index="porsches") -> None:
+    """
+    Permet de clear le client elasticsearch pour de nouvelles recherches
+    :param es_client: client elasticSearch
+    :param index: index à clear
+    """
+    # delete_by_query afin de supprimer tous les éléments
+    response = es_client.delete_by_query(
+        index=index,
+        body={
+            "query": {
+                "match_all": {}
+            }
+        }
+    )
+
+    # Rafraîchissement de l'index pour s'assurer que les modifications soient
+    # faites
+    es_client.indices.refresh(index=index)
+
+
+def indexation(es_client, documents) -> None:
+    """
+    Permet de créer un index ElasticSearch
+    :param es_client: client ElasticSearch
+    :param documents: à indexer
+    """
+    index_name = 'porsches'
+
+    try:
+        # Si l'index existe déjà
+        if not es.indices.exists(index=index_name):
+            es_client.indices.create(index=index_name)
+
+        # Réalise l'indexation
+        response = bulk(es_client, generate_data(documents))
+
+        # S'assure que les index sont à jour
+        es_client.indices.refresh(index=index_name)
+    except Exception as error:
+        print(error)
+
+
 def search_porsche_model(index_name, l_100_min, l_100_max, power_ch_min,
                          power_ch_max, top_speed_min, top_speed_max,
                          acceleration_min, acceleration_max, porsche_price_min,
-                         porsche_price_max):
-
+                         porsche_price_max) -> dict:
+    """
+    Permet de sélectionner les documents en fonction des paramètres
+    :param index_name: nom de l'index
+    :param l_100_min: parlant
+    :param l_100_max: parlant
+    :param power_ch_min: parlant
+    :param power_ch_max: parlant
+    :param top_speed_min: parlant
+    :param top_speed_max: parlant
+    :param acceleration_min: parlant
+    :param acceleration_max: parlant
+    :param porsche_price_min: parlant
+    :param porsche_price_max: parlant
+    :return: un dictionnaire avec le contenu des données filtrées
+    """
     # Définit selon les paramètres d'entrée la range pour chaque filtre
     range_filter = [
         {"range": {"l_100": {"gte": l_100_min,
@@ -33,11 +106,9 @@ def search_porsche_model(index_name, l_100_min, l_100_max, power_ch_min,
     # Permet d'effectuer la recherche elasticsearch
     results = es.search(
         index=index_name,
-        body=query,
-        size=10)
+        body=query
+    )
 
     hits = results.get('hits', {}).get('hits', [])
 
-    print(hits)
-
-    return None
+    return hits
