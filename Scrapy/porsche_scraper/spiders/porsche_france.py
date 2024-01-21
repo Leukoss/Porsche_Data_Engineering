5a7ff0eb38ff2ee58ec2e76e3faf298885888a1e
Verzys <lucas.saliorliange.pro@gmail.com>
2024-01-21 23:54:06 +0100

    Ajout du scraping final

--- Scrapy/porsche_scraper/spiders/porsche_france.py
+++ Scrapy/porsche_scraper/spiders/porsche_france.py
@@ -1,26 +1,23 @@
 """
-    Module permettant le scraping du site de Porsche et la mise en relation avec
-    la base de données mongo afin de stocker les données scrapées
+Module for web scraping Porsche's website and connecting to the MongoDB database to store scraped data.
 
-    Auteurs : Lucas SALI--ORLIANGE
-    Date : janvier 2024
+Authors: Lucas SALI--ORLIANGE
+Date: January 2024
 """
 import scrapy
 import re
 
 from ..items import PorscheScraperItem
 
-
 class PorscheFranceSpider(scrapy.Spider):
     """
-        Class Docstring :
-        Spider permettant le scrapping du site de Porsche afin de récupérer
-        diverses informations concernant les modèles de Porsche disponibles.
+    Class Docstring:
+    Spider for web scraping Porsche's website to retrieve various information about available Porsche models.
 
-        Attributes :
-            name (str) : nom du spider.
-            allowed_domains (list) : liste des noms de domaine à crawl.
-            start_url (list) : liste des URLs de départ pour le spider.
+    Attributes:
+        name (str): Name of the spider.
+        allowed_domains (list): List of allowed domains to crawl.
+        start_url (list): List of starting URLs for the spider.
     """
     name = "porsche_france"
     allowed_domains = ["www.porsche.com"]
@@ -28,19 +25,19 @@ class PorscheFranceSpider(scrapy.Spider):
 
     def parse(self, response) -> None:
         """
-        Permet de réaliser le parsing depuis la première page web
-        :param response: lien du site contenant les modèles de porsche
+        Parses the first web page to retrieve different types of Porsche models.
+        :param response: Link to the site containing Porsche models.
         """
-        # Récupère les différents types de modèles possibles
+        # Retrieve different types of possible models
         models_dividers = response.css('.m-14-model-series-divider')
 
-        # Pour chaque type (Modèles 718, 718 Cayman GT4 RS, etc.)
+        # For each type (Models 718, 718 Cayman GT4 RS, etc.)
         for divider in models_dividers:
             model_type_url = divider.css('a::attr(href)').get()
 
-            # Si le lien existe, accès à la page contenant les modèles liés
+            # If the link exists, access the page containing related models
             if model_type_url:
-                # Permet d'ouvrir ladite page
+                # Open the mentioned page
                 yield response.follow(
                     model_type_url,
                     callback=self.parse_informations
@@ -48,16 +45,15 @@ class PorscheFranceSpider(scrapy.Spider):
 
     def parse_informations(self, models_response) -> None:
         """
-        Permet d'accéder aux différentes informations et stock les infos dans la
-        BDD
-        :param models_response: lien menant à une catégorie de porsche
+        Accesses various information and stores it in the database.
+        :param models_response: Link leading to a Porsche category.
         """
-        # Récupère tous les conteneurs pour chaque modèle dudit type
+        # Retrieve all containers for each model of that type
         models = models_response.css('.m-364-module-specs-content')
 
-        # Pour chaque modèle disponible
+        # For each available model
         for model in models:
-            # On récupère toutes les informations qui nous intéressent
+            # Retrieve all desired information
             dict_details = self.parse_details(model)
 
             yield PorscheScraperItem(
@@ -73,15 +69,15 @@ class PorscheFranceSpider(scrapy.Spider):
     @staticmethod
     def parse_name(model_response) -> str:
         """
-        Permet de récupérer le nom du modèle depuis un nom en plusieurs éléments
-        :param model_response: contient la response contenant le nom
-        :return: str contenant le nom du modèle
+        Retrieves the model name from a name with multiple elements.
+        :param model_response: Contains the response containing the name.
+        :return: Model's name as a string.
         """
-        # On récupère le nom de notre modèle
+        # Retrieve the model's name
         model_name_parts = model_response.css(
             '.m-364-module-headline--title *::text').getall()
 
-        # On nettoie et récupère le nom complet
+        # Clean and retrieve the complete name
         full_model_name = ' '.join(
             part.strip() for part in model_name_parts if
             part.strip())
@@ -91,10 +87,9 @@ class PorscheFranceSpider(scrapy.Spider):
     @staticmethod
     def parse_price(model_response) -> str:
         """
-        Permet de récupérer le prix du modèle depuis un prix en plusieurs
-        éléments
-        :param model_response: contient la response contenant le prix
-        :return: prix du modèle
+        Retrieves the model's price from a price with multiple elements.
+        :param model_response: Contains the response containing the price.
+        :return: Model's price.
         """
         price = ''.join(re.findall(r'\d+', (model_response.css(
             '.m-364-module-headline--copy::text').get())))[:-2]
@@ -104,40 +99,40 @@ class PorscheFranceSpider(scrapy.Spider):
     @staticmethod
     def parse_l100(model_response) -> str:
         """
-        Permet d'obtenir la valeur du nombre de litres pour 100 km
-        :param model_response: contient la response contenant le l/100
-        :return: str l_100
+        Retrieves the value of liters per 100 km.
+        :param model_response: Contains the response containing l/100.
+        :return: L/100 as a string.
         """
         return model_response.css('span.b-eco__value::text').get()
 
     @staticmethod
     def parse_url_image(model_response) -> str:
         """
-        Permet d'obtenir l'image de la porsche
-        :param model_response: contient la response contenant l'url de l'image
-        :return: str image_url
+        Retrieves the Porsche image.
+        :param model_response: Contains the response containing the image URL.
+        :return: Image URL as a string.
         """
         return model_response.css('img.m-364-module-image::attr(data-image-src)').get()
 
     @staticmethod
     def parse_details(model_response):
         """
-        Permet de récupérer la puissance, l'accélération et la vitesse maximale
-        :param model_response: contient les 3 informations
-        :return: un dictionnaire avec les 3 valeurs
+        Retrieves power, acceleration, and maximum speed.
+        :param model_response: Contains all three pieces of information.
+        :return: A dictionary with three values.
         """
-        # Récupère l'ensemble des informations
+        # Retrieve all information
         infos = model_response.css(
             '.m-364-techspecs-center .m-364-module-specs')
 
         return {
-            # Récupère la puissance
+            # Retrieve power
             'power': infos.css('.m-364-module-specs-data--title::text').get(),
-            # Récupère l'accélération
+            # Retrieve acceleration
             'acceleration': infos.css(
                 '.m-364-module-specs-data:nth-child(2) '
                 '.m-364-module-specs-data--title::text').get(),
-            # Récupère la vitesse maximale
+            # Retrieve maximum speed
             'top_speed': infos.css(
                 '.m-364-module-specs-data:nth-child(3) '
                 '.m-364-module-specs-data--title::text').get()
