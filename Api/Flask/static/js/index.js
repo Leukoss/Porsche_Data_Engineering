68cf083f1b0f58c590d193ccf88d59892419bc7a
Verzys <lucas.saliorliange.pro@gmail.com>
2024-01-21 22:29:21 +0100

    ajout du fichier index.js avec les commentaires en anglais

--- Api/Flask/static/js/index.js
+++ Api/Flask/static/js/index.js
@@ -1,4 +1,7 @@
+// Get the HTML element with the id 'allForme'
 const allForme = document.getElementById('allForme');
+
+// Define an array of form element IDs
 const formElements = [
     'price-min', 'price-max',
     'l-100-min', 'l-100-max',
@@ -7,9 +10,15 @@ const formElements = [
     'accel-min', 'accel-max'
 ];
 
+// Iterate through the form element IDs using forEach
 formElements.forEach(elementId => {
+    // Get the HTML element with the current elementId
     const element = document.getElementById(elementId);
+
+    // Add an event listener for the 'change' event on the element
     element.addEventListener('change', () => {
+        // When the element's value changes, submit the 'allForme' form
         allForme.submit();
     });
 });
+
